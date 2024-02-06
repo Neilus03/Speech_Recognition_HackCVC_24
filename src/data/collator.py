345ca763a0c1e460f5c6f8314342378ec5b7b698
neil <neildlf@gmail>
2024-02-06 15:27:42 +0100

    done

--- src/data/collator.py
+++ src/data/collator.py
@@ -1,10 +1,21 @@
-#Collector function to collate the data from the data source
+#Collator function to collate the data from the data source
 
-# Path: src/data/datasets.py
 
-from torch.utils.data import Dataset
-import numpy as np
-import os
-
-class LipReadingDataset(Dataset):
+class LipReadingCollator:
+    def __init__(self, transforms = None, tokenizer = None):
+        self.transforms = transforms
+        self.tokenizer = tokenizer
     
+    def collate_fn(batch):
+        '''
+        This function will take in a batch of data and collate it into a single tensor
+        The batch is a list of dictionaries, where each dictionary contains the keys 'transcription', 'face_frames', and 'keypoints'
+        The collate function should return a dictionary with the same keys, but the values should be a single tensor, each with the 
+        proper transformation applied and properly tokenized
+        
+        The key 'transcription' should be tokenized using the tokenizer, the key 'face_frames' should be transformed using the 
+        transforms and then stacked into a single tensor The key 'keypoints' should be transformed using the transforms and
+        then stacked into a single tensor
+        '''
+        
+        
--- src/data/whisper_transcription.py
+++ src/data/whisper_transcription.py
@@ -5,9 +5,6 @@ import random
 
 model = whisper.load_model("base")
 
-#result = model.transcribe("/data3fast/users/group02/videos/The Fear of Success Dr Marty Hauff at TEDxYouth@WISS_trimmed.mp3")
-#print(result["text"])
-
 directori = "/data3fast/users/group02/videos/tracks/"
 
 files = list(os.walk(directori))
@@ -19,5 +16,21 @@ for carpeta_actual, carpetes, fitxers in tqdm(files):
             result = model.transcribe(ruta_completa)
             with open(os.path.join(carpeta_actual, 'transcription.txt'), 'w') as file:
                 file.write(result["text"])
+                
 
+'''
+#In english below:   
 
+model = whisper.load_model("base")
+
+directory = "/data3fast/users/group02/videos/tracks/"
+files = list(os.walk(directory))
+
+random.shuffle(files)
+for folder, subfolders, files in tqdm(files):
+    for file in files:
+        full_path = os.path.join(folder, file)
+        if full_path.endswith('.mp3'):
+            result = model.transcribe(full_path)
+            with open(os.path.join(folder, 'transcription.txt'), 'w') as file:
+                file.write(result["text"])'''
